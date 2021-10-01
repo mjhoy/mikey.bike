fcc917c5cf8e1e6625c8bbf8f6879e7622dc4cd1
Michael Hoy <mjh@mjhoy.com>
2021-10-01 07:20:20 -0400

    hash file at path function

---
+++ src/FileHash.hs
@@ -0,0 +1,13 @@
+{-# LANGUAGE BangPatterns #-}
+
+module FileHash where
+
+import qualified Crypto.Hash.SHA256 as SHA256
+import qualified Data.ByteString.Lazy as BSL
+import qualified Data.ByteString.Char8 as BS8
+import qualified Data.ByteString.Base16 as Base16
+
+hash :: FilePath -> IO String
+hash path = do
+  !h <- SHA256.hashlazy <$> BSL.readFile path
+  pure $ BS8.unpack $ Base16.encode h
