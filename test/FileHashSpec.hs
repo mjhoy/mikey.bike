d5de609f97eed6c158bbac27bd105b4a99823501
Michael Hoy <mjh@mjhoy.com>
2021-10-01 08:00:29 -0400

    test test failure

--- test/FileHashSpec.hs
+++ test/FileHashSpec.hs
@@ -8,4 +8,4 @@ spec = do
   describe "hash" $ do
     it "should correctly hash a file" $ do
       res <- hash "test/fixtures/test-file-hash.json"
-      res `shouldBe` "b9cd2605ea75293b16b892a97c5e4b0bc18f3dafd0cbdf897c80258d57415c80"
+      res `shouldBe` "this should fail"
